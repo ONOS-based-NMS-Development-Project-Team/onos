dc3b74171014b6f37bf989c8be681b3d1708c4a5
zzp110 <670614177@qq.com>
2018-08-28 17:22:59 +0800

    修改NNAlgorithmConfig.java内容

--- core/api/src/main/java/org/onosproject/soon/mlmodel/NNAlgorithmConfig.java
+++ core/api/src/main/java/org/onosproject/soon/mlmodel/NNAlgorithmConfig.java
@@ -12,69 +12,70 @@ import static org.onosproject.soon.mlmodel.MLAlgorithmType.FCNNModel;
  */
 public class NNAlgorithmConfig extends MLModelConfig {
 
-    private int neuronsInputLayer;
-    private int hiddenLayers;
-    //列表中的值为隐藏层的神经元个数
-    private List<Integer> neuronsHiddenLayers;
-    private int neuronsOutputLayer;
-    private double learningRate;
-    private List<Double> weights;
-    private List<Double> bias;
-    private String activation;
+    private int inpLayNeuNum; // 输入层神经元个数
+    private int hidLayNum; // 隐藏层层数
+    //列表中的值为各个隐藏层的神经元个数
+    private List<Integer> hidLayNeuNum;
+    private int outpLayNeuNum;//输出神经元个数
+    private double learningRate;//学习率，区间为(0, 1)
+    private int weights;//权重初始化方法，1代表
+    private int bias;//偏置初始化方法，1代表
+    private List<Integer> activation;//每层的激活函数，1代表ReLU，2代表sigmoid，3代表LeakyReLU，4代表PReLU，5代表tanh
     private int steps;
-    private String loss;
+    // loss function definition
+    private String lossFun;//损失函数
 
     public NNAlgorithmConfig(MLAlgorithmType type) {
         super(type);
     }
 
-    public NNAlgorithmConfig(MLAlgorithmType type, int neuronsInputLayer, int hiddenLayers,
-                             List<Integer> neuronsHiddenLayers, int neuronsOutputLayer, double learningRate,
-                             List<Double> weights, List<Double> bias, String activation, int steps, String loss) {
+    public NNAlgorithmConfig(MLAlgorithmType type, int inpLayNeuNum, int hidLayNum,
+                             List<Integer> hidLayNeuNum, int outpLayNeuNum, double learningRate,
+                             int weights, int bias, List<Integer> activation,
+                             int steps, String lossFun) {
         super(type);
-        this.neuronsInputLayer = neuronsInputLayer;
-        this.hiddenLayers = hiddenLayers;
-        this.neuronsHiddenLayers = neuronsHiddenLayers;
-        this.neuronsOutputLayer = neuronsOutputLayer;
+        this.inpLayNeuNum = inpLayNeuNum;
+        this.hidLayNum = hidLayNum;
+        this.hidLayNeuNum = hidLayNeuNum;
+        this.outpLayNeuNum = outpLayNeuNum;
         this.learningRate = learningRate;
         this.weights = weights;
         this.bias = bias;
         this.activation = activation;
         this.steps = steps;
-        this.loss = loss;
+        this.lossFun = lossFun;
     }
 
-    //getter and setter
-    public int getNeuronsInputLayer() {
-        return neuronsInputLayer;
+    public int getInpLayNeuNum() {
+        return inpLayNeuNum;
     }
 
-    public void setNeuronsInputLayer(int neuronsInputLayer) {
-        this.neuronsInputLayer = neuronsInputLayer;
+    public void setInpLayNeuNum(int inpLayNeuNum) {
+        this.inpLayNeuNum = inpLayNeuNum;
     }
 
-    public int getHiddenLayers() {
-        return hiddenLayers;
+    public int getHidLayNum() {
+        return hidLayNum;
     }
 
-    public void setHiddenLayers(int hiddenLayers) {
-        this.hiddenLayers = hiddenLayers;
+    public void setHidLayNum(int hidLayNum) {
+        this.hidLayNum = hidLayNum;
     }
 
-    public List<Integer> getNeuronsHiddenLayers() {
-        return neuronsHiddenLayers;
+    public List<Integer> getHidLayNeuNum() {
+        return hidLayNeuNum;
     }
 
-    public void setNeuronsHiddenLayers(List<Integer> neuronsHiddenLayers) {
-        this.neuronsHiddenLayers = neuronsHiddenLayers;
+    public void setHidLayNeuNum(List<Integer> hidLayNeuNum) {
+        this.hidLayNeuNum = hidLayNeuNum;
     }
 
-    public int getNeuronsOutputLayer() {
-        return neuronsOutputLayer;
+    public int getOutpLayNeuNum() {
+        return outpLayNeuNum;
     }
 
-    public void setNeuronsOutputLayer(int neuronsOutputLayer) {
-        this.neuronsOutputLayer = neuronsOutputLayer;
+    public void setOutpLayNeuNum(int outpLayNeuNum) {
+        this.outpLayNeuNum = outpLayNeuNum;
     }
 
     public double getLearningRate() {
@@ -85,27 +86,27 @@ public class NNAlgorithmConfig extends MLModelConfig {
         this.learningRate = learningRate;
     }
 
-    public List<Double> getWeights() {
+    public int getWeights() {
         return weights;
     }
 
-    public void setWeights(List<Double> weights) {
+    public void setWeights(int weights) {
         this.weights = weights;
     }
 
-    public List<Double> getBias() {
+    public int getBias() {
         return bias;
     }
 
-    public void setBias(List<Double> bias) {
+    public void setBias(int bias) {
         this.bias = bias;
     }
 
-    public String getActivation() {
+    public List<Integer> getActivation() {
         return activation;
     }
 
-    public void setActivation(String activation) {
+    public void setActivation(List<Integer> activation) {
         this.activation = activation;
     }
 
@@ -117,12 +118,11 @@ public class NNAlgorithmConfig extends MLModelConfig {
         this.steps = steps;
     }
 
-    public String getLoss() {
-        return loss;
+    public String getLossFun() {
+        return lossFun;
     }
 
-    public void setLoss(String loss) {
-        this.loss = loss;
+    public void setLossFun(String lossFun) {
+        this.lossFun = lossFun;
     }
-
 }
